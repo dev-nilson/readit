e2708c76c1443f1d84d429a02465ff85a2eb252e
Denilson <dlemus@harding.edu>
2023-02-09 20:40:12 -0600

    ♻️ Refactor avatar component

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
 
-function Avatar() {
+type AvatarProps = {
+  seed?: string;
+  large?: boolean;
+};
+
+function Avatar({ seed, large }: AvatarProps) {
   return (
-    <div className="relative h-10 w-10 rounded-full border-gray-300 bg-white">
+    <div
+      className={`relative h-10 w-10 rounded-full border-gray-300 bg-white ${
+        large && "h-20 w-20"
+      }`}
+    >
       <Image
-        src={`https://api.dicebear.com/5.x/pixel-art/png?seed=Denilson`}
+        src={`https://api.dicebear.com/5.x/pixel-art/png?seed=${seed}`}
         fill={true}
         alt=""
       />
--- src/components/PostBox/PostBox.tsx
+++ src/components/PostBox/PostBox.tsx
@@ -2,9 +2,9 @@ import Avatar from "../Avatar/Avatar";
 
 function PostBox() {
   return (
-    <form>
+    <form className="sticky top-15 z-50 bg-white border border-gray-300 p-2">
       <div className="flex items-center space-x-3">
-        <Avatar />
+        <Avatar seed="" />
         <input
           className="bg-gray-50 p-2 pl-5 outline-none rounded-md flex-1"
           type="text"
