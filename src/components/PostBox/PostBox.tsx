1ee778c0229cc8e3ca60667acb7802ec946a68e6
Denilson <dlemus@harding.edu>
2023-02-10 13:10:46 -0600

    ✨ Add category input

--- src/components/PostBox/PostBox.tsx
+++ src/components/PostBox/PostBox.tsx
@@ -3,7 +3,7 @@ import Avatar from "../Avatar/Avatar";
 
 type Post = {
   advice: string;
-  subpost: string;
+  category: string;
 };
 
 function PostBox() {
@@ -19,12 +19,26 @@ function PostBox() {
       <div className="flex items-center space-x-3">
         <Avatar seed="" />
         <input
-          {...(register("advice", { required: true }))}
+          {...register("advice", { required: true })}
           className="bg-gray-50 p-2 pl-5 outline-none rounded-md flex-1"
           type="text"
           placeholder="Post a piece of advice"
         />
       </div>
+
+      {!!watch("advice") && (
+        <div>
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Category</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register("category", { required: true })}
+              type="text"
+              placeholder="Select a category"
+            />
+          </div>
+        </div>
+      )}
     </form>
   );
 }
