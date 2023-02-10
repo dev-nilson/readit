edff8fddca8a828313b62ccc809ae6df1cf3438d
Denilson <dlemus@harding.edu>
2023-02-09 19:29:02 -0600

    ✨ Set apollo client

---
+++ apollo-client.js
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: import.meta.NEXT_PUBLIC_CLIENT_URI,
+  headers: {
+    Authorization: `Apikey ${import.meta.NEXT_PUBLIC_API_KEY}`
+  },
+  cache: new InMemoryCache(),
+});
