abe4546ee9f48fe829414ec3d24712b6b7b694b7
Denilson <dlemus@harding.edu>
2023-02-09 20:50:10 -0600

    ✨ Control input box

--- src/components/PostBox/PostBox.tsx
+++ src/components/PostBox/PostBox.tsx
@@ -1,11 +1,26 @@
+import { useForm } from "react-hook-form";
 import Avatar from "../Avatar/Avatar";
 
+type Post = {
+  title: string;
+  text: string;
+  subpost: string;
+};
+
 function PostBox() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Post>();
+
   return (
     <form className="sticky top-15 z-50 bg-white border border-gray-300 p-2">
       <div className="flex items-center space-x-3">
         <Avatar seed="" />
         <input
+          {...(register("text"), { required: true })}
           className="bg-gray-50 p-2 pl-5 outline-none rounded-md flex-1"
           type="text"
           placeholder="Post a piece of advice"
