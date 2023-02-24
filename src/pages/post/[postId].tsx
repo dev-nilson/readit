7fdcb5409a752bb08e32e09a02e73e6e40a8644b
Denilson <dlemus@harding.edu>
2023-02-24 15:10:27 -0600

    ✨ Add comment section for post pages

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -16,6 +16,16 @@ function PostPage() {
   return (
     <div className="mx-auto max-w-5xl">
       <Post post={post} />
+      <div className="-mt-5 rounded-b-md border border-t-0 border-gray-300 bg-white p-5 pl-16">
+        <p className="text-sm">Comment as</p>
+        <form className="flex flex-col max-w-5xl space-y-3">
+          <textarea
+            className="h-24 rounded-md border border-gray-200 p-2 pl-4 outline-none disabled:bg-gray-50"
+            placeholder="Add a comment"
+          />
+          <button type="submit" className="rounded-full bg-orange-600 px-3 py-2 font-semibold text-white disabled:bg-gray-200">Comment</button>
+        </form>
+      </div>
     </div>
   );
 }
