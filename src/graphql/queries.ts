17d85d82870bf58e729dc7680552ab857efd2e8d
Denilson <dlemus@harding.edu>
2023-02-25 00:03:57 -0600

    ✨ Add query to get votes by id

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -102,3 +102,15 @@ export const GET_POST_BY_ID = gql`
     }
   }
 `;
+
+export const GET_VOTES_BY_POST_ID = gql`
+  query MyQuery($post_id: ID!) {
+    getVotesByPostId(post_id: $post_id) {
+      id
+      upvote
+      username
+      post_id
+      created_at
+    }
+  }
+`;
