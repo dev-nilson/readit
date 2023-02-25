d3fad711f362b0971622efd7608db807f5e005c9
Denilson <dlemus@harding.edu>
2023-02-25 00:02:23 -0600

    ✨ Display post comments

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -1,3 +1,4 @@
+import Avatar from "@/components/Avatar/Avatar";
 import Post from "@/components/Post/Post";
 import { ADD_COMMENT } from "@/graphql/mutations";
 import { GET_POST_BY_ID } from "@/graphql/queries";
@@ -5,6 +6,7 @@ import { useMutation, useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
+import TimeAgo from "react-timeago";
 
 type FormData = {
   comment: string;
@@ -73,6 +75,27 @@ function PostPage() {
           </form>
         </div>
       )}
+      <div className="-my-5 rounded-b-md border border-t-0 border-gray-300 bg-white py-5 px-10 mb-5">
+        <hr className="py-2" />
+        {post?.comments.map((comment) => (
+          <div
+            className="relative flex items-center space-x-2 space-y-5"
+            key={comment.id}
+          >
+            <hr className="absolute top-10 h-14 left-7 z-0 border" />
+            <div className="z-50">
+              <Avatar />
+            </div>
+            <div className="flex flex-col">
+              <p className="py-2 text-xs text-gray-400">
+                <span className="font-semibold text-gray-600">{comment.username}</span>{" • "}
+                <TimeAgo date={comment.created_at} />
+              </p>
+              <p>{comment.text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
