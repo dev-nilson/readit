fd89fff93df6617dcd9c51f1d6e6e7330acd8fbe
Denilson <dlemus@harding.edu>
2023-02-25 22:55:36 -0600

    🐛 Hide comments section when loading

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -55,47 +55,52 @@ function PostPage() {
     <div className="mx-auto max-w-5xl">
       <Post post={post} />
       {post && (
-        <div className="-mt-5 rounded-b-md border border-t-0 border-gray-300 bg-white p-5 pl-16">
-          <p className="text-sm">Comment as</p>
-          <form
-            className="flex flex-col max-w-5xl space-y-3"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <textarea
-              {...register("comment")}
-              className="h-24 rounded-md border border-gray-200 p-2 pl-4 outline-none disabled:bg-gray-50"
-              placeholder="Add a comment"
-            />
-            <button
-              type="submit"
-              className="rounded-full bg-orange-600 px-3 py-2 font-semibold text-white disabled:bg-gray-200"
+        <>
+          <div className="-mt-5 rounded-b-md border border-t-0 border-gray-300 bg-white p-5 pl-16">
+            <p className="text-sm">Comment as</p>
+            <form
+              className="flex flex-col max-w-5xl space-y-3"
+              onSubmit={handleSubmit(onSubmit)}
             >
-              Comment
-            </button>
-          </form>
-        </div>
-      )}
-      <div className="-my-5 rounded-b-md border border-t-0 border-gray-300 bg-white py-5 px-10 mb-5">
-        <hr className="py-2" />
-        {post?.comments.map((comment) => (
-          <div
-            className="relative flex items-center space-x-2 space-y-5"
-            key={comment.id}
-          >
-            <hr className="absolute top-10 h-14 left-7 z-0 border" />
-            <div className="z-50">
-              <Avatar />
-            </div>
-            <div className="flex flex-col">
-              <p className="py-2 text-xs text-gray-400">
-                <span className="font-semibold text-gray-600">{comment.username}</span>{" • "}
-                <TimeAgo date={comment.created_at} />
-              </p>
-              <p>{comment.text}</p>
-            </div>
+              <textarea
+                {...register("comment")}
+                className="h-24 rounded-md border border-gray-200 p-2 pl-4 outline-none disabled:bg-gray-50"
+                placeholder="Add a comment"
+              />
+              <button
+                type="submit"
+                className="rounded-full bg-orange-600 px-3 py-2 font-semibold text-white disabled:bg-gray-200"
+              >
+                Comment
+              </button>
+            </form>
           </div>
-        ))}
-      </div>
+          <div className="-my-5 rounded-b-md border border-t-0 border-gray-300 bg-white py-5 px-10 mb-5">
+            <hr className="py-2" />
+            {post?.comments.map((comment) => (
+              <div
+                className="relative flex items-center space-x-2 space-y-5"
+                key={comment.id}
+              >
+                <hr className="absolute top-10 h-14 left-7 z-0 border" />
+                <div className="z-50">
+                  <Avatar />
+                </div>
+                <div className="flex flex-col">
+                  <p className="py-2 text-xs text-gray-400">
+                    <span className="font-semibold text-gray-600">
+                      {comment.username}
+                    </span>
+                    {" • "}
+                    <TimeAgo date={comment.created_at} />
+                  </p>
+                  <p>{comment.text}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 }
