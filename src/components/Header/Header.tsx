7f015ac64e582d74807f38c1f868749119a93798
Denilson <dlemus@harding.edu>
2023-02-27 13:46:35 -0600

    📱 Update header responsiveness

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import { HomeIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 function Header() {
   return (
-    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm space-x-10">
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm space-x-10 items-center justify-center">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
         <Link href="/">
           <Image
@@ -17,12 +17,12 @@ function Header() {
         </Link>
       </div>
 
-      <div className="flex items-center mx-7 xl:min-w-[300px]">
+      <div className="flex items-center mx-7">
         <HomeIcon className="h-5 w-5" />
         <p className="flex-1 ml-2 hidden md:inline">Home</p>
       </div>
 
-      <form className="flex-1 items-center space-x-2 rounded-full border border-gray-200 bg-gray-100 px-3 py-1 max-w-2xl hidden md:flex">
+      <form className="flex-1 items-center space-x-2 rounded-full border border-gray-200 bg-gray-100 px-3 py-1 max-w-2xl hidden sm:flex">
         <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
         <input
           className="flex-1 bg-transparent outline-none"
