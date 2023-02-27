bfb61b8eea8c869040b429949efb491803fdfc73
Denilson <dlemus@harding.edu>
2023-02-27 13:29:05 -0600

    ✨ Add widget for top categories

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { Inter } from "@next/font/google";
-import Header from "@/components/Header/Header";
 import Head from "next/head";
 import PostBox from "@/components/PostBox/PostBox";
 import Feed from "@/components/Feed/Feed";
@@ -19,6 +18,12 @@ export default function Home() {
           <PostBox />
           <div className="flex">
             <Feed />
+            <div className="sticky top-36 mx-5 mt-9 h-fit hidden min-w-[300px] rounded-md border border-gray-300 bg-white lg:inline">
+              <p className="uppercase text-xs p-3 font-semibold">Popular Categories</p>
+              <div>
+
+              </div>
+            </div>
           </div>
         </main>
       </div>
